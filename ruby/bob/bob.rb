bb5a9b5430c39d13c71fb1c5d2d9d50300a7f126
Rian Rainey <rian@rianrainey.com>
2013-10-14 19:27:37 -0500

    Refactoring unnecessary code and classes.

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -2,40 +2,31 @@ class Bob
   def new
   end
 
-  def hey(command)
+  def hey command
     statement = Statement.new(command)
-    response  = Response.new
 
-    if statement.is_valid_statement?
-      case
-      when statement.is_yelling? && statement.contains_words?
-        response.to_yelling
-      when statement.is_question?
-        response.to_question
-      else
-        response.default
-      end
-    elsif statement.blank?
-      response.to_saying_nothing
+    case
+    when statement.yelling? && statement.contains_words?
+      "Woah, chill out!" 
+    when statement.question?
+      "Sure."
+    when statement.blank?
+      "Fine. Be that way!"
     else
-      response.default
+      "Whatever."
     end
   end
 end
 
 class Statement < String
-  def new(value)
+  def new value
   end
 
-  def is_valid_statement?
-    /\w(\!|\.|\?)/ =~ self || self.contains_words?
-  end
-
-  def is_question?
+  def question?
     self[-1] === "?"
   end
 
-  def is_yelling?
+  def yelling?
     self === self.upcase 
   end
 
@@ -43,29 +34,7 @@ class Statement < String
     /([a-zA-Z]+\s*[a-zA-Z]*)+\w/ =~ self
   end
 
-  # Implementing my implementation of Rails' .blank? method
   def blank?
     self.strip.length === 0
-  end
-end
-
-class Response
-  def new
-  end
-
-  def to_yelling
-    "Woah, chill out!" 
-  end
-
-  def to_question
-    "Sure."
-  end
-
-  def to_saying_nothing
-    "Fine. Be that way!"
-  end
-
-  def default
-    "Whatever."
   end
 end

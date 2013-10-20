af0e1d324c9ecbf9e1b6d48ced6efb9f3d031446
Rian Rainey <rian@rianrainey.com>
2013-10-20 15:16:49 -0500

    Simplified #word_count

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -4,8 +4,16 @@ class Phrase
   end
 
   def word_count
-    count_of_words = Hash.new
     words = get_words(@phrase)
+    count_words(words)
+  end
+
+  def get_words(phrase)
+    phrase.downcase.split(/[^\w+']/).delete_if(&:empty?)
+  end
+
+  def count_words(words)
+    count_of_words = Hash.new
     words.each do |word|
       if count_of_words.key?(word)
         count_of_words[word] = count_of_words[word] + 1
@@ -14,10 +22,6 @@ class Phrase
       end
     end
     count_of_words
-  end
-
-  def get_words(phrase)
-    phrase.downcase.split(/[^\w+']/).delete_if(&:empty?)
   end
 
 end

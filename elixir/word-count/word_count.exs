60f42cf70c2d796f39eb708deff0c2a1bdcff693
Rian Rainey <hello@rian.me>
2015-06-17 12:39:50 -0500

    Elixir word count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule Words do
   @doc """
   Count the number of words in the sentence.
@@ -6,6 +8,15 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
+    regex = ~r/(_|[^\w\p{Pd}])+/u
 
+    sentence
+    |> String.downcase
+    |> String.split(regex, trim: true)
+    |> Enum.reduce(Map.new, &make_dict_counter/2)
+  end
+
+  defp make_dict_counter(word, dict) do
+    Dict.update(dict, word, 1, &(1 + &1))
   end
 end

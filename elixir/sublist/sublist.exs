a4da4fc0f7cc41db548e77b16f8266bad3375378
Rian Rainey <rrainey@covermymeds.com>
2016-02-11 19:41:49 -0600

    First four tests complete

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,7 +3,13 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) do
-
+  def compare([], []), do: :equal
+  def compare([], _),  do: :sublist
+  def compare(_, []),  do: :superlist
+  def compare( [ a | a_tail ], [ b | b_tail ]) do
+    cond do
+      a != b -> :unequal
+      a == b -> :equal
+    end
   end
 end
